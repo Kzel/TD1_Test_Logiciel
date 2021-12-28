f00376487a215c5052760133e3a9e5337a2798e7
Kzel <baoyining12@gmail.com>
2021-12-29 00:46:41 +0100

    add create_db drop_db , add_user test and prototype

--- funcs.py
+++ funcs.py
@@ -1,2 +1,14 @@
 import sqlite3
 
+def create_db(db_path):
+	connect = sqlite3.connect(db_path)
+	cursor = connect.cursor()
+	cursor.execute('CREATE TABLE Users ([id_user] INTEGER PRIMARY KEY,[username] text UNIQUE, [password] text, [spublickey] text, [sprivatekey] text, [epublickey] text,[eprivatekey] text)')
+	connect.commit()
+
+def drop_db(db_path):
+    connect = sqlite3.connect(db_path)
+    cursor = connect.cursor()
+    cursor.execute('DROP TABLE IF EXISTS Users')
+    connect.commit()
+
--- test_funcs.py
+++ test_funcs.py
@@ -1,8 +1,15 @@
 import funcs
 import unittest
 
-class TestFuncs(unittest.TestCase):
+db_path = 'ex2.db'
 
+class TestFuncs(unittest.TestCase):
+	def test_add_user(self):
+		self.assertEqual(funcs.add_user(db_path,'BA!','1234567','this is spublickey1','this is sprivatekey1', 'this is epublickey1', 'thisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekey'), True)
+		self.assertEqual(funcs.add_user(db_path,'YiningBAO','Ab!123456','this is spublickey2','this is sprivatekey2', 'this is epublickey2', 'this is eprivatekey2'), True)
+  
 	
 if __name__ == '__main__':
-	unittest.main()
+    funcs.drop_db(db_path)
+    funcs.create_db(db_path)
+    unittest.main()
