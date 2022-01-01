16419eef3f67c95ff1472fa2fe25e2263cfbeee7
Kzel <baoyining12@gmail.com>
2022-01-01 21:38:25 +0100

    modified key function

--- test_funcs.py
+++ test_funcs.py
@@ -5,8 +5,8 @@ db_path = 'ex2.db'
 
 class TestFuncs(unittest.TestCase):
 	def test_add_user(self):
-		self.assertEqual(funcs.add_user(db_path,'BA!','1234567','this is spublickey1','this is sprivatekey1', 'this is epublickey1', 'thisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekey'), True)
-		self.assertEqual(funcs.add_user(db_path,'YiningBAO','Ab!123456','this is spublickey2','this is sprivatekey2', 'this is epublickey2', 'this is eprivatekey2'), True)
+		self.assertEqual(funcs.add_user(db_path,'BA!','1234567','this is spublickey1'.rjust(128)[:128],'this is sprivatekey1'.rjust(128)[:128], 'this is epublickey1'.rjust(128)[:128], 'this is eprivatekey1'.rjust(129)[:129]), True)
+		self.assertEqual(funcs.add_user(db_path,'YiningBAO','Ab!123456','this is spublickey2'.rjust(128)[:128],'this is sprivatekey2'.rjust(128)[:128], 'this is epublickey2'.rjust(128)[:128], 'this is eprivatekey2'.rjust(128)[:128]), True)
   
 	def test_login(self):
 		self.assertEqual(funcs.log(db_path,'BA!','Ab!123456'), False)
@@ -17,20 +17,20 @@ class TestFuncs(unittest.TestCase):
 		self.assertEqual(funcs.get_password(db_path, 'YiningBAO'), 'Ab!123456')
   
 	def test_spublickey(self):
-		self.assertEqual(funcs.get_spubkey(db_path,'BA!'),'this is spublickey1')
-		self.assertEqual(funcs.get_spubkey(db_path,'YiningBAO'),'this is spublickey2')
+		self.assertEqual(funcs.get_spubkey(db_path,'BA!'),'this is spublickey1'.rjust(128)[:128])
+		self.assertEqual(funcs.get_spubkey(db_path,'YiningBAO'),'this is spublickey2'.rjust(128)[:128])
 	
 	def test_sprilickey(self):
-		self.assertEqual(funcs.get_sprikey(db_path,'BA!'),'this is sprivatekey1')
-		self.assertEqual(funcs.get_sprikey(db_path,'YiningBAO'),'this is sprivatekey2')
+		self.assertEqual(funcs.get_sprikey(db_path,'BA!'),'this is sprivatekey1'.rjust(128)[:128])
+		self.assertEqual(funcs.get_sprikey(db_path,'YiningBAO'),'this is sprivatekey2'.rjust(128)[:128])
 	
 	def test_epublickey(self):
-		self.assertEqual(funcs.get_epubkey(db_path,'BA!'),'this is epublickey1')
-		self.assertEqual(funcs.get_epubkey(db_path,'YiningBAO'),'this is epublickey2')
+		self.assertEqual(funcs.get_epubkey(db_path,'BA!'),'this is epublickey1'.rjust(128)[:128])
+		self.assertEqual(funcs.get_epubkey(db_path,'YiningBAO'),'this is epublickey2'.rjust(128)[:128])
 	
 	def test_eprilickey(self):
-		self.assertEqual(funcs.get_eprikey(db_path,'BA!'),'thisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekeythisiseprivatekey')
-		self.assertEqual(funcs.get_eprikey(db_path,'YiningBAO'),'this is eprivatekey2')
+		self.assertEqual(funcs.get_eprikey(db_path,'BA!'),'this is eprivatekey1'.rjust(129)[:129])
+		self.assertEqual(funcs.get_eprikey(db_path,'YiningBAO'),'this is eprivatekey2'.rjust(128)[:128])
   
 	def test_verify_username_length(self):
 		self.assertEqual(funcs.verify_username_length(db_path,'BA!'), False)
